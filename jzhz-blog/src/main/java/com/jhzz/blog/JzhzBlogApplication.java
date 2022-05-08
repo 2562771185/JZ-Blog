2ec0ecbc47ceaad6cd14cf04dcc872c1cb1d8694
JHZZ_mjz <m2562771185@163.com>
2022-05-08 12:31:24 +0800

    2 commit

---
+++ jzhz-blog/src/main/java/com/jhzz/blog/JzhzBlogApplication.java
@@ -0,0 +1,19 @@
+package com.jhzz.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* @author: Huanzhi
+ * \* Date: 2022/5/8
+ * \* Time: 12:00
+ * \* Description:
+ * \
+ */
+@SpringBootApplication
+public class JzhzBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JzhzBlogApplication.class, args);
+    }
+}
