79fb8e61397efac082570b4e31476ad5817e7cc2
JHZZ_mjz <m2562771185@163.com>
2022-05-08 12:38:52 +0800

    2 lll

---
+++ jzhz-blog/src/main/java/com/jhzz/blog/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.jhzz.blog.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* @author: Huanzhi
+ * \* Date: 2022/5/8
+ * \* Time: 12:38
+ * \* Description:
+ * \
+ */
+@RestController
+public class HelloController {
+
+
+}
